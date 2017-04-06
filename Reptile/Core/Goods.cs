05852d6dfd92514db26ff48d494c36af612f2b4f
林时帅 <circle_july@163.com>
2017-04-06 17:31:58 +0800

    Core 添加

---
+++ Reptile/Core/Goods.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core
+{
+    public class Goods
+    {
+        public string Name { get; set; }
+
+        public  Shop Shop { get; set; }
+    }
+}
---
+++ Reptile/Core/Shop.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core
+{
+    public class Shop
+    {
+        public string Name { get; set; }
+
+        public ICollection<Goods> GoodsCollection { get; set; }
+    }
+}
